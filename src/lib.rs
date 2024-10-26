b3c15606023b59e924f0e2caf65700cf66a5059b
Alex Saveau <saveau.alexandre+tmp@gmail.com>
2024-10-26 16:56:39 -0400

    v0.4.11

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub fn fmt() {
         let mut command = process::Command::new(env::var("CARGO").unwrap());
         command.args(["fmt", "--all"]);
 
-        let config = "--config=version=Two,imports_granularity=Crate,\
+        let config = "--config=style_edition=2024,imports_granularity=Crate,\
                       group_imports=StdExternalCrate,use_field_init_shorthand=true,\
                       format_code_in_doc_comments=true,format_macro_bodies=true,\
                       format_macro_matchers=true,format_strings=true,wrap_comments=true";
